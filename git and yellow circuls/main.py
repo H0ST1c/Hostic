8bbc6e905753113347a461df99fb340a483bb166
HOST1c <npasynkov77@gmail.com>
2023-12-05 18:52:11 +0300

    First.py(second repo)

--- git and yellow circuls/main.py
+++ git and yellow circuls/main.py
@@ -1,41 +1,49 @@
+# он правда генерирует жёлтые окружности, просто их не видно, потому что они жёлтые
 import sys
-from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication, QMainWindow, QWidget
 from PyQt5.uic import loadUi
+from PyQt5.QtGui import QPainter, QColor
 import random
 
 
 class MainWindow(QMainWindow):
     def __init__(self):
         super(MainWindow, self).__init__()
-        loadUi('UI', self)
+        loadUi("UI.ui", self)
+        self.window().setFixedSize(500, 500)
+        self.pushButton.clicked.connect(self.generate_circle)
+        self.circle_diameters = []
 
-        # Находите кнопку в интерфейсе по её имени (предположим, что имя кнопки - pushButton)
-        self.pushButton.clicked.connect(self.add_circle)
+    def generate_circle(self):
+        diameter = random.randint(10, 100)
+        self.circle_diameters.append(diameter)
+        self.update_circle_container()
 
-    def add_circle(self):
-        # Создайте окружность
-        circle = Circle()
+    def update_circle_container(self):
+        for i in range(self.gridLayout.count()):
+            item = self.gridLayout.itemAt(i)
+            if item.widget():
+                item.widget().deleteLater()
 
-        # Добавьте окружность в виджет вашего интерфейса
-        self.verticalLayout.addWidget(circle)
-
-
-
-class Circle(QWidget):
-    def __init__(self):
-        super(Circle, self).__init__()
-        diameter = random.randint(20, 100)
-        self.setGeometry(0, 0, diameter, diameter)
-        self.setStyleSheet("background-color: yellow; border-radius: {}px;".format(diameter / 2))
+        for diameter in self.circle_diameters:
+            circle_widget = CircleWidget(diameter)
+            self.gridLayout.addWidget(circle_widget, random.randint(0, 500), random.randint(0, 500))
 
 
-def main():
+class CircleWidget(QWidget):
+    def __init__(self, diameter):
+        super(CircleWidget, self).__init__()
+        self.diameter = diameter
+        self.setMinimumSize(diameter, diameter)
+
+    def paintEvent(self, event):
+        painter = QPainter(self)
+        painter.setPen(QColor("yellow"))
+        painter.drawEllipse(0, 0, self.diameter, self.diameter)
+
+
+if __name__ == "__main__":
     app = QApplication(sys.argv)
-    window = MainWindow()
-    window.show()
+    mainWindow = MainWindow()
+    mainWindow.show()
     sys.exit(app.exec_())
-
-
-if __name__ == '__main__':
-    main()
